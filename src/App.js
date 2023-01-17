a8601b0baa17ff3dfc345c29ffd55b871af9bd78
Eyasu <eyasutew1@gmail.com>
2023-01-17 12:45:18 +0300

    Sign in sign up forgot password

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 // import logo from './logo.svg';
 // import './App.css';
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
+
 function App() {
   return (
-
     <>
-      <h1 className='text-2xl bg-red-400'>Hello</h1>
+    <Router>
+      <Header/>
+
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/sign-in" element={<SignIn/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
+        <Route path="/forgot-password" element={<ForgotPassword/>}/>
+        <Route path="/offers" element={<Offers/>}/>
+      </Routes>
+    </Router>
     </>
+
+    
+    // <>
+    //   <h1 className='text-2xl bg-red-400'>Hello</h1>
+    // </>
+
+
     // <div className="App">
     //   <header className="App-header">
     //     <img src={logo} className="App-logo" alt="logo" />
